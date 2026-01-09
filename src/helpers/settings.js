8a54294a2112ea93fa00f442b9e5107d2785d08b
Andrej Kudriavcev <andrej@kudriavcev.info>
2026-01-09 21:57:23 +1100

    Add console log for inferred BASE_URL in settings.js

--- src/helpers/settings.js
+++ src/helpers/settings.js
@@ -55,6 +55,8 @@ const host =
 const defaultProtocol = (host === 'localhost') ? 'http' : 'https';
 const inferredUrl = `${defaultProtocol}://${host}`;
 
+console.log("Inferred BASE_URL as:", inferredUrl);
+
 /** Application configuration object.
  * Builds up the configuration from environment variables with defaults,
  * and can be used in other modules of the application.
