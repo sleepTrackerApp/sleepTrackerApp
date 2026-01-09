be023d115a83b20c3a517822475ba1a36f8f72da
22203853 <s222038631@deakin.edu.au>
2026-01-09 19:15:50 +1100

    Updated summary functionfor dates

--- src/services/weeklySummaryService.js
+++ src/services/weeklySummaryService.js
@@ -6,16 +6,18 @@
 const summary = require("../models/Summary")
 const sleepEntriesService = require("./sleepEntriesService")
 
+// Get all weekly summary entries from model.
 async function getAllWeeklySummary(userId){
     return await summary.find({ userId })
 }
+
 
 async function createWeeklySummary(userId) {
     const entries = await sleepEntriesService.getAllSleepEntries(userId);
     if(!entries.length) return null;
 
-
-    const startDate = new Date();
+    const today = new Date()
+    const startDate = new Date(today);
     startDate.setDate(startDate.getDate() - 6);
     startDate.setHours(0, 0, 0, 0);
 
@@ -29,14 +31,13 @@ async function createWeeklySummary(userId) {
     const totalHours = weeklyEntries.reduce((sum, e) => sum + e.hours, 0);
     const avgHours = totalHours / weeklyEntries.length;
 
-    const summary = await Summary.findOneAndUpdate(
+    const summary = await summary.findOneAndUpdate(
         { userId, date: today },
         { userId, date: today, avgHours },
         { upsert: true, new: true }
     );
 
     return summary;
-
 }
 
 module.exports = {
