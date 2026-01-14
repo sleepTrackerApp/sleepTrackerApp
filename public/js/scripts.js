e15c7d8c9b9e9c27cebf5aef0216970c5cb48132
Akashdeep Singh <akashjaura016@gmail.com>
2026-01-14 21:39:58 +1100

    Redesign dashboard UI and interactions

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
   $('.sidenav').sidenav();
+
+  const toast = document.getElementById('saveToast');
+  const triggers = document.querySelectorAll('.js-confirm-save');
+  if (toast && triggers.length) {
+    triggers.forEach((btn) => {
+      btn.addEventListener('click', (e) => {
+        e.preventDefault();
+        toast.classList.add('show');
+        setTimeout(() => toast.classList.remove('show'), 2000);
+      });
+    });
+  }
 });
