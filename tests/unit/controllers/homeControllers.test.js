59e3fb419525c66a81a0eeaa7c37f2869df62add
mivo1101 <vochautrami.work@gmail.com>
2026-01-17 22:01:24 +1100

    Update homeController unit test for Contentful mock

--- tests/unit/controllers/homeControllers.test.js
+++ tests/unit/controllers/homeControllers.test.js
@@ -1,24 +1,34 @@
 const { expect } = require('chai');
 const sinon = require('sinon');
 const { renderHome } = require('../../../src/controllers/homeControllers');
+const { contentfulService } = require('../../../src/services');
 
 describe('Home controller tests', () => {
   afterEach(() => {
     sinon.restore();
   });
 
-  it('renders home page with correct title and activeMenu', () => {
+  it('renders home page with correct title and activeMenu', async () => {
     const req = {};
     const res = {
       render: sinon.stub(),
     };
 
-    renderHome(req, res);
+    const getArticlesStub = sinon.stub(contentfulService, 'getArticles').resolves([
+      { title: 'Article 1', slug: 'a1', image: 'img1.jpg', excerpt: '...' },
+      { title: 'Article 2', slug: 'a2', image: 'img2.jpg', excerpt: '...' }
+    ]);
+
+    await renderHome(req, res);
 
     expect(
       res.render.calledOnceWithExactly('pages/home', {
         title: 'Wake Up Truly Alive',
         activeMenu: 'home',
+        articles: [
+          { title: 'Article 1', slug: 'a1', image: 'img1.jpg', excerpt: '...' },
+          { title: 'Article 2', slug: 'a2', image: 'img2.jpg', excerpt: '...' }
+        ]
       })
     ).to.be.true;
   });
