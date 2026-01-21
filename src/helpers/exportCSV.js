6ec156ce8f202858130d673c2704b7e8269cc263
22203853 <s222038631@deakin.edu.au>
2026-01-21 19:35:11 +1100

    Export CSV Helper JS

--- src/helpers/exportCSV.js
+++ src/helpers/exportCSV.js
@@ -3,8 +3,21 @@
  */
 
 
-function csvExport(data){
-    if (!data || !data.length) return 'No Data';
+function csvExport(entries){
+    if (!entries) {return 'No Data'};
+    
+    const headers = Object.keys(entries[0]).join(',');
+
+    const rows = entries.map(entry => {
+        return Object.values(entry)
+            .map(value => {
+                if (value == null) return '';
+                return `"${String(value).replace(/"/g, '""')}"`;
+            })
+            .join(',');
+    });
+
+return [headers, ...rows].join('\n');
 
 }
 
