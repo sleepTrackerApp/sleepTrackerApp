5da0c0eb0cf783df8bc5cc02924338095bed66de
Andrej Kudriavcev <andrej@kudriavcev.info>
2026-01-21 19:56:31 +1100

    fix: remove email/password login handler and streamline logout functionality (#52)

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -8,9 +8,7 @@ const { authControllers } = require('../controllers');
 const router = express.Router();
 
 router.get('/login', authControllers.login);
-router.post('/login', authControllers.emailPasswordLogin);
 router.get('/logout', authControllers.logout);
 
 module.exports = router;
-
 
