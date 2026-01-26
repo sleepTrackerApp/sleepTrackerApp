e9b25bef4063422eb82fa027b71f05186d19bc34
22203853 <s222038631@deakin.edu.au>
2026-01-26 19:06:46 +1100

    Update delete all user data controller to call main delete service

--- src/controllers/deleteUserController.js
+++ src/controllers/deleteUserController.js
@@ -4,4 +4,21 @@
  */
 const deleteUserData  = require("../services/deleteUserService")
 
+async function deleteAllUserData (req, res, next){
+    try{
+        const user = res.locals.userRecord;
+        const deleteAll = await deleteUserData.deleteAllData(userId);
 
+        res.status(202).json({
+            success: true,
+            message: "User Account has been deleted"
+        });
+    } catch (error){
+        next(error);
+    }
+}
+
+
+module.exports = {   
+    deleteAllUserData
+};
